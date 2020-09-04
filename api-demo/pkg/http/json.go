2c718020b184e81f54b8178b146cabeefa408e46
Breno S. Caraccio <breno.sherrer@icloud.com>
2020-09-04 11:37:19 +0100

    fix superfluous call to writeHeader

--- api-demo/pkg/http/json.go
+++ api-demo/pkg/http/json.go
@@ -8,17 +8,17 @@ import (
 func WriteJSON(w http.ResponseWriter, payload interface{}) {
 	encoder := json.NewEncoder(w)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 
 	if err := encoder.Encode(payload); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	} else {
-		w.WriteHeader(http.StatusOK)
+		panic(err)
 	}
 }
 
 func WriteError(w http.ResponseWriter, err error, code int) {
 	encoder := json.NewEncoder(w)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
 
 	payload := struct {
 		Error string `json:"error"`
@@ -28,8 +28,6 @@ func WriteError(w http.ResponseWriter, err error, code int) {
 
 	// TODO: the error should be typed to figure if it was a server error or an user error
 	if err := encoder.Encode(payload); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	} else {
-		w.WriteHeader(code)
+		panic(err)
 	}
 }
